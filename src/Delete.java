66e7257f96ee344a73cec5a42db3b72a6e7c342c
Ostrokrzew <ostrokrzew@protonmail.com>
2019-05-31 09:15:47 +0200

    next little delete bug fix

--- src/Delete.java
+++ src/Delete.java
@@ -37,7 +37,7 @@ public class Delete {
 	}
 	
 	public static String deleteAccount(String param) {
-        String delete = String.format("DELETE FROM konto WHERE id_konto == %s", param);
+        String delete = String.format("DELETE FROM konto WHERE id_k == %s", param);
         try {
             SQLite.stat.execute(delete);
         } catch (SQLException e) {
